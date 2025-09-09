357f5606b5dbc99b8df42a338677ed8afbbf3643
nicky <nicolas_cernadas@hotmail.com>
2025-09-09 20:09:11 -0300

    init/svelte-project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import favicon from "$lib/assets/favicon.svg";
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<!-- Pasa 1 componente por $props() y genera el html -->
+{@render children?.()}
