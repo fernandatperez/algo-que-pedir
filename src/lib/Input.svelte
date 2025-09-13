208338f914fbbcdfa3da050e86d72160fcf83409
nicky <nicolas_cernadas@hotmail.com>
2025-09-13 18:48:48 -0300

    props() rune instead export

--- src/lib/Input.svelte
+++ src/lib/Input.svelte
@@ -1,19 +1,13 @@
 <script lang="ts">
 import "$lib/css/components-css/input.css";
 
-// let {
-//   label_for = "",
-//   label_text = "",
-//   input_type = "text",
-//   input_placeholder = "",
-//   input_id = ""
-// } = $props()
-
-export let label_for
-export let label_text
-export let input_type
-export let input_placeholder
-export let input_id
+let {
+  label_for = "",
+  label_text = "",
+  input_type = "text",
+  input_placeholder = "",
+  input_id = ""
+} = $props()
 
 </script>
 
