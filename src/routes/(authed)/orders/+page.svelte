504e60f08dd7296c80b060689b5113d40a613ef8
CatalinaCorrea <catalinayazmincorrea@gmail.com>
2025-09-15 23:55:08 -0300

    Filtro pedidos por tabs

--- src/routes/(authed)/orders/+page.svelte
+++ src/routes/(authed)/orders/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
     import "$lib/css/components-css/user-details.css";
     import "$lib/css/components-css/buttons.css";
     import "$lib/css/pages-css/3-orders.css";
@@ -8,6 +8,7 @@
     import pizzaImg from "$lib/assets/img/pizza.png";
     import ensaladaImg from "$lib/assets/img/ensalada.png";
 
+    // Ejemplo de pedido
     let single_pedido = $state({
         pedidoId: 12345,
         cliente: {
@@ -56,26 +57,34 @@
         pago: "Tarjeta de crédito",
     });
 
+    // Todos los pedidos (ejemplo)
     let pedidos = $state([single_pedido, single_pedido, single_pedido, single_pedido, single_pedido, single_pedido, single_pedido]);
 
-    // let filtro = "pendiente";
-    // onclick={filtro = "pendiente"}
-    // onclick={filtro = "preparado"}
-    // onclick={filtro = "entregado"}
-    // onclick={filtro = "cancelado"}
+    let estado = $state("Pendiente");
+    
+    // Filtrar pedidos por estado
+    const filtrarPedidos = (estado: string) => pedidos.filter((pedido) => pedido.estado === estado)
+    
+    let pedidosFiltrados = $derived(filtrarPedidos(estado));
+
+    const handleStateChange = (newState: string) => {
+        estado = newState;
+        console.log("Estado cambiado a:", estado);
+        // pedidosFiltrados = filtrarPedidos(estado);
+    };
 
 </script>
 
 <!-- Tabs and content -->
 <main class="title-tabs-grid">
+    <h1 class="title">Pedidos actuales</h1>
     <section class="line">
         <div class="title-tabs">
-            <h1 class="title">Pedidos actuales</h1>
             <nav class="tabs">
-                <button class="btn-empty tab active">Pendientes</button>
-                <button class="btn-empty tab">Preparados</button>
-                <button class="btn-empty tab">Entregados</button>
-                <button class="btn-empty tab">Cancelados</button>
+                <button onclick={() => handleStateChange("Pendiente")} class="btn-empty tab active">Pendientes</button>
+                <button onclick={() => handleStateChange("Preparado")} class="btn-empty tab">Preparados</button>
+                <button onclick={() => handleStateChange("Entregado")} class="btn-empty tab">Entregados</button>
+                <button onclick={() => handleStateChange("Cancelado")} class="btn-empty tab">Cancelados</button>
             </nav>
         </div>
     </section>
@@ -83,274 +92,9 @@
     <!-- Orders grid -->
     <section class="main-grid">
         <!-- Single order -->
-        {#each pedidos as pedido}
+        {#each pedidosFiltrados as pedido}
             <OrderCard {pedido} />
-            <!-- <a href="/orders-details" class="grid-item">
-                <header class="order">Pedido #{pedido.pedidoId}</header>
-
-                <div class="user">
-                    <i class="ph ph-user-circle"></i>
-                    <div class="user-info">
-                        <div class="name">{pedido.cliente.nombre}</div>
-                        <div class="username">
-                            <strong>usuario:</strong> {pedido.cliente.usuario}
-                        </div>
-                    </div>
-                </div>
-
-                <p class="details">Hora: 12:30 PM | Artículos: 2 | Total: $25</p>
-
-                <address class="address-container">
-                    <div class="pin-container">
-                        <i class="ph ph-map-pin"></i>
-                    </div>
-                    <div class="address-coordinates">
-                        <span class="address"
-                            ><strong>{pedido.cliente.direccion.calle}</strong></span
-                        >
-                        <div class="coordinates">Lat: {pedido.cliente.direccion.latitud}, Long: {pedido.cliente.direccion.longitud}</div>
-                    </div>
-                </address>
-
-                <div class="payment">
-                    <i class="ph ph-credit-card"></i>
-                    <span class="payment-text">Pago con {pedido.pago}</span>
-                </div>
-
-                <div class="action-container">
-                    <button class="btn btn-primary"> Preparar </button>
-                </div>
-            </a> -->
         {/each}
 
-
-
-        <!-- Single order -->
-        <!-- <article class="grid-item">
-            <header class="order">Pedido #1234</header>
-
-            <div class="user">
-                <i class="ph ph-user-circle"></i>
-                <div class="user-info">
-                    <div class="name">Sofía Miller</div>
-                    <div class="username">
-                        <strong>usuario:</strong> smiller2006
-                    </div>
-                </div>
-            </div>
-
-            <p class="details">Hora: 12:30 PM | Artículos: 2 | Total: $25</p>
-
-            <address class="address-container">
-                <div class="pin-container">
-                    <i class="ph ph-map-pin"></i>
-                </div>
-                <div>
-                    <div class="address">
-                        <strong>Av siempre viva 555</strong>
-                    </div>
-                    <div class="coordinates">Lat: 40.7128, Long: -74.0060</div>
-                </div>
-            </address>
-
-            <div class="payment">
-                <i class="ph ph-credit-card"></i>
-                <span class="payment-text">Pago con tarjeta de crédito</span>
-            </div>
-
-            <div class="action-container">
-                <button class="btn btn-primary">Preparar</button>
-            </div>
-        </article> -->
-
-        <!-- Single order -->
-        <!-- <article class="grid-item">
-            <header class="order">Pedido #1234</header>
-
-            <div class="user">
-                <i class="ph ph-user-circle"></i>
-                <div class="user-info">
-                    <div class="name">Sofía Miller</div>
-                    <div class="username">
-                        <strong>usuario:</strong> smiller2006
-                    </div>
-                </div>
-            </div>
-
-            <p class="details">Hora: 12:30 PM | Artículos: 2 | Total: $25</p>
-
-            <address class="address-container">
-                <div class="pin-container">
-                    <i class="ph ph-map-pin"></i>
-                </div>
-                <div>
-                    <div class="address">
-                        <strong>Av siempre viva 555</strong>
-                    </div>
-                    <div class="coordinates">Lat: 40.7128, Long: -74.0060</div>
-                </div>
-            </address>
-
-            <div class="payment">
-                <i class="ph ph-credit-card"></i>
-                <span class="payment-text">Pago con tarjeta de crédito</span>
-            </div>
-
-            <div class="action-container">
-                <button class="btn btn-primary">Preparar</button>
-            </div>
-        </article> -->
-
-        <!-- Single order -->
-        <!-- <article class="grid-item">
-            <header class="order">Pedido #1234</header>
-
-            <div class="user">
-                <i class="ph ph-user-circle"></i>
-                <div class="user-info">
-                    <div class="name">Sofía Miller</div>
-                    <div class="username">
-                        <strong>usuario:</strong> smiller2006
-                    </div>
-                </div>
-            </div>
-
-            <p class="details">Hora: 12:30 PM | Artículos: 2 | Total: $25</p>
-
-            <address class="address-container">
-                <div class="pin-container">
-                    <i class="ph ph-map-pin"></i>
-                </div>
-                <div>
-                    <div class="address">
-                        <strong>Av siempre viva 555</strong>
-                    </div>
-                    <div class="coordinates">Lat: 40.7128, Long: -74.0060</div>
-                </div>
-            </address>
-
-            <div class="payment">
-                <i class="ph ph-credit-card"></i>
-                <span class="payment-text">Pago con tarjeta de crédito</span>
-            </div>
-
-            <div class="action-container">
-                <button class="btn btn-primary">Preparar</button>
-            </div>
-        </article> -->
-
-        <!-- Single order -->
-        <!-- <article class="grid-item">
-            <header class="order">Pedido #1234</header>
-
-            <div class="user">
-                <i class="ph ph-user-circle"></i>
-                <div class="user-info">
-                    <div class="name">Sofía Miller</div>
-                    <div class="username">
-                        <strong>usuario:</strong> smiller2006
-                    </div>
-                </div>
-            </div>
-
-            <p class="details">Hora: 12:30 PM | Artículos: 2 | Total: $25</p>
-
-            <address class="address-container">
-                <div class="pin-container">
-                    <i class="ph ph-map-pin"></i>
-                </div>
-                <div>
-                    <div class="address">
-                        <strong>Av siempre viva 555</strong>
-                    </div>
-                    <div class="coordinates">Lat: 40.7128, Long: -74.0060</div>
-                </div>
-            </address>
-
-            <div class="payment">
-                <i class="ph ph-credit-card"></i>
-                <span class="payment-text">Pago con tarjeta de crédito</span>
-            </div>
-
-            <div class="action-container">
-                <button class="btn btn-primary">Preparar</button>
-            </div>
-        </article> -->
-
-        <!-- Single order -->
-        <!-- <article class="grid-item">
-            <header class="order">Pedido #1234</header>
-
-            <div class="user">
-                <i class="ph ph-user-circle"></i>
-                <div class="user-info">
-                    <div class="name">Sofía Miller</div>
-                    <div class="username">
-                        <strong>usuario:</strong> smiller2006
-                    </div>
-                </div>
-            </div>
-
-            <p class="details">Hora: 12:30 PM | Artículos: 2 | Total: $25</p>
-
-            <address class="address-container">
-                <div class="pin-container">
-                    <i class="ph ph-map-pin"></i>
-                </div>
-                <div>
-                    <div class="address">
-                        <strong>Av siempre viva 555</strong>
-                    </div>
-                    <div class="coordinates">Lat: 40.7128, Long: -74.0060</div>
-                </div>
-            </address>
-
-            <div class="payment">
-                <i class="ph ph-credit-card"></i>
-                <span class="payment-text">Pago con tarjeta de crédito</span>
-            </div>
-
-            <div class="action-container">
-                <button class="btn btn-primary">Preparar</button>
-            </div>
-        </article> -->
-
-        <!-- Single order -->
-        <!-- <article class="grid-item">
-            <header class="order">Pedido #1234</header>
-
-            <div class="user">
-                <i class="ph ph-user-circle"></i>
-                <div class="user-info">
-                    <div class="name">Sofía Miller</div>
-                    <div class="username">
-                        <strong>usuario:</strong> smiller2006
-                    </div>
-                </div>
-            </div>
-
-            <p class="details">Hora: 12:30 PM | Artículos: 2 | Total: $25</p>
-
-            <address class="address-container">
-                <div class="pin-container">
-                    <i class="ph ph-map-pin"></i>
-                </div>
-                <div>
-                    <div class="address">
-                        <strong>Av siempre viva 555</strong>
-                    </div>
-                    <div class="coordinates">Lat: 40.7128, Long: -74.0060</div>
-                </div>
-            </address>
-
-            <div class="payment">
-                <i class="ph ph-credit-card"></i>
-                <span class="payment-text">Pago con tarjeta de crédito</span>
-            </div>
-
-            <div class="action-container">
-                <button class="btn btn-primary">Preparar</button>
-            </div>
-        </article> -->
     </section>
 </main>
