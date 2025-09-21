29c530a0dfe68a88d838305018dad88c930a1365
Maxi <machugab@gmail.com>
2025-09-21 14:02:56 -0300

    componente basico creado

---
+++ src/lib/MenuItem.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  let { img, alt, nombre, descripcion, precio } = $props();
+</script>
+
+<article class="meal">
+  <div class="imgcontainer">
+    <img src={img} {alt} class="image-meal" />
+  </div>
+  <div class="text-meal text-wrapper">
+    <h4 class="ellipsis-text">{nombre}</h4>
+    <p class="gray-text hide-on-mobile ellipsis-text">
+      {descripcion}
+    </p>
+  </div>
+  <div class="price-meal">{precio}</div>
+</article>
--- src/routes/(authed)/menu/+page.svelte
+++ src/routes/(authed)/menu/+page.svelte
@@ -5,6 +5,8 @@
   import "$lib/css/components-css/buttons.css";
   import "$lib/css/pages-css/5-menu.css";
   //   <link rel="stylesheet" href="../css/style.css" /> esto no hace falta pero no me acuerdo en donde se importa
+
+  import MenuItem from "$lib/MenuItem.svelte";
 </script>
 
 <main class="container-column">
@@ -17,86 +19,45 @@
     </div>
     <h2 class="subtitle">Platos disponibles</h2>
     <div class="container-column content-section">
-      <article class="meal">
-        <div class="imgcontainer">
-          <img
-            src="src/lib/assets/img/spagettis.png"
-            alt="spagettis"
-            class="image-meal"
-          />
-        </div>
-        <div class="text-meal text-wrapper">
-          <h4 class="ellipsis-text">Pasta con albondigas</h4>
-          <p class="gray-text hide-on-mobile ellipsis-text">
-            Deliciosa pasta con salsa de tomates y albondigas de cerdo
-          </p>
-        </div>
-        <div class="price-meal">$12.99</div>
-      </article>
-      <article class="meal">
-        <div class="imgcontainer">
-          <img
-            src="src/lib/assets/img/hamburguesa2.jpg"
-            alt="hamburguesa"
-            class="image-meal"
-          />
-        </div>
-        <div class="text-meal text-wrapper">
-          <h4 class="ellipsis-text">Hamburguesa con queso y panceta</h4>
-          <p class="gray-text hide-on-mobile ellipsis-text">
-            Combo de hamburguesa con papas y bebida
-          </p>
-        </div>
-        <div class="price-meal">$9.99</div>
-      </article>
-      <article class="meal">
-        <div class="imgcontainer">
-          <img
-            src="src/lib/assets/img/ensalada.png"
-            alt="ensalada"
-            class="image-meal"
-          />
-        </div>
-        <div class="text-meal text-wrapper">
-          <h4 class="ellipsis-text">Ensalada de la Huerta</h4>
-          <p class="gray-text hide-on-mobile ellipsis-text">
-            Ensalada fresca con hojas mixtas y vinagreta
-          </p>
-        </div>
-        <div class="price-meal">$7.50</div>
-      </article>
-      <article class="meal">
-        <div class="imgcontainer">
-          <img
-            src="src/lib/assets/img/pizza.png"
-            alt="pizza"
-            class="image-meal"
-          />
-        </div>
-        <div class="text-meal text-wrapper">
-          <h4 class="ellipsis-text">Pizza con mozzarella y tomate</h4>
-          <p class="gray-text hide-on-mobile ellipsis-text">
-            Pizza a la piedra con salsa de tomates frescos y extra queso
-          </p>
-        </div>
-        <div class="price-meal">$11.75</div>
-      </article>
-      <article class="meal">
-        <div class="imgcontainer">
-          <img
-            src="src/lib/assets/img/salmon.png"
-            alt="salmon"
-            class="image-meal"
-          />
-        </div>
-        <div class="text-meal text-wrapper">
-          <h4 class="ellipsis-text">Salmon con vegetales grillados</h4>
-          <p class="gray-text hide-on-mobile ellipsis-text">
-            Salmon fresco a la plancha acompañado de vegetales
-          </p>
-        </div>
-        <div class="price-meal">$14.25</div>
-      </article>
+      <MenuItem
+        img="src/lib/assets/img/spagettis.png"
+        alt="spagettis"
+        nombre="Pasta con albondigas"
+        descripcion="Deliciosa pasta con salsa de tomates y albondigas de cerdo"
+        precio="$12.99"
+      />
+
+      <MenuItem
+        img="src/lib/assets/img/hamburguesa2.jpg"
+        alt="hamburguesa"
+        nombre="Hamburguesa con queso y panceta"
+        descripcion="Combo de hamburguesa con papas y bebida"
+        precio="$9.99"
+      />
+
+      <MenuItem
+        img="src/lib/assets/img/ensalada.png"
+        alt="ensalada"
+        nombre="Ensalada de la Huerta"
+        descripcion="Ensalada fresca con hojas mixtas y vinagreta"
+        precio="$7.50"
+      />
+
+      <MenuItem
+        img="src/lib/assets/img/pizza.png"
+        alt="pizza"
+        nombre="Pizza con mozzarella y tomate"
+        descripcion="Pizza a la piedra con salsa de tomates frescos y extra queso"
+        precio="$11.75"
+      />
+
+      <MenuItem
+        img="src/lib/assets/img/salmon.png"
+        alt="salmon"
+        nombre="Salmon con vegetales grillados"
+        descripcion="Salmon fresco a la plancha acompañado de vegetales"
+        precio="$14.25"
+      />
     </div>
   </div>
 </main>
