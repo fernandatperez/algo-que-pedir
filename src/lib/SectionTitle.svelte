d44da0add428df87889fe289dd40f98a50827648
Fernanda Perez <fernanda.tperez@outlook.com>
2025-09-22 18:23:31 -0300

    elimino componentizacion innecesaria

--- src/lib/SectionTitle.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-  export let text = "";
-</script>
-
-<!-- esta componentizacion esta pensada para los titulos que se comparten en todas las paginas-->
-<h1 class="header-title">{text}</h1>
