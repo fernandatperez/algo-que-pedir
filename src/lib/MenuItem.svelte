0c4ad49d46242c627d5e9f5bbf94a8a626b88734
Maxi <machugab@gmail.com>
2025-09-27 18:18:00 -0300

    ruteo de dish-edit

--- src/lib/MenuItem.svelte
+++ src/lib/MenuItem.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import type { MenuItemType } from "$lib/type/menu-item.ts";
+  import { goto } from "$app/navigation";
 
   const { menuitem } = $props<{ menuitem: MenuItemType }>();
   // aca decis que menuitem es de tipo MenuItemType que es la interfaz
@@ -11,19 +12,21 @@
   // }
 </script>
 
-<article class="meal">
-  <div class="imgcontainer">
-    <img
+  <!-- <button onclick={()=> goto(`/dish-edit/${menuitem.id}`)}>hola</button> -->
+
+<a class="meal" href={`/dish-edit/${menuitem.id}`}>
+    <div class="imgcontainer">
+      <img
       src="src/lib/assets/img/{menuitem.imagen}"
       alt={menuitem.alt}
       class="image-meal"
-    />
-  </div>
-  <div class="text-meal text-wrapper">
-    <h4 class="ellipsis-text">{menuitem.nombre}</h4>
-    <p class="gray-text hide-on-mobile ellipsis-text">
-      {menuitem.descripcion}
-    </p>
-  </div>
-  <div class="price-meal">{menuitem.precio}</div>
-</article>
+      />
+    </div>
+    <div class="text-meal text-wrapper">
+      <h4 class="ellipsis-text">{menuitem.nombre}</h4>
+      <p class="gray-text hide-on-mobile ellipsis-text">
+        {menuitem.descripcion}
+      </p>
+    </div>
+    <div class="price-meal">{menuitem.precio}</div>
+</a>
