c0936f4e4e869e5e8208ff1cbe97ef62c7ee2a38
dcreyes <dcossettinireyes@estudiantes.unsam.edu.ar>
2025-09-30 10:02:20 -0300

    fix navegacion entre pagina para las tres vistas

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,76 +1,104 @@
 <script lang="ts">
-  import "$lib/css/components-css/navBar.css";
+  import "$lib/css/components-css/navBar.css"
   // Guarda la ruta activa
-  let activeRoute = "orders";
-  import { page } from "$app/state";
-
+  import { page } from "$app/state"
+  
+  let activeRoute = "orders"
 
   function setActive(route: string) {
-    // activeRoute = route;
+    activeRoute = route;
   }
 
   const routes = [
-    { path: "/orders", label: "Pedidos", key: "orders" },
-    { path: "/menu", label: "Menú", key: "menu" },
-    { path: "/ingredients", label: "Ingredientes", key: "ingredients" },
-    { path: "/store-profile", label: "Cuentas", key: "store-profile" }
-  ];
+    { path: "/orders", label: "Pedidos", key: "orders", icon: "ph-shopping-cart" },
+    { path: "/menu", label: "Menú", key: "menu", icon: "ph-fork-knife" },
+    { path: "/ingredients", label: "Ingredientes", key: "ingredients", icon: "ph-bowl-food" },
+    { path: "/store-profile", label: "Cuentas", key: "store-profile", icon: "ph-user", specialIcon: "ph-user-circle" }
+  ]
+
+  const isActive = (path: string) => {
+    return path === '/'
+      ? page.url.pathname === '/'
+      : page.url.pathname === path || page.url.pathname.startsWith(`${path}/`)
+  }
 </script>
 
 <!-- hay que cambiar las rutas despues -->
 <nav class="options-and-icon">
   {#each routes as route}
     <a 
-      href={route.path} 
-      class="btn-empty {activeRoute === route.key ? 'active' : ''}"
-      onclick={() => setActive(route.key)}
-    >
-      {route.label}
+    class:active={isActive(route.path)}
+    aria-current={page.url.pathname === route.path ? 'page' : undefined}
+    href={route.path}>{route.label}
     </a>
   {/each}
+
+  <!-- Texto del link activo (solo tablet) -->
+  <span class="activeLink">
+    {#each routes as route}
+      {#if isActive(route.path)}
+        <a class:active={isActive(route.path)} href={route.path}>
+          {route.label}
+        </a>
+      {/if}
+    {/each}
+  </span>
 
 <div class="dropdown">
   <!-- <i class="ph ph-list hidden"></i> -->
   <label id="nav-hamburger" class="hidden">
     <input type="checkbox" id="hamburger-toggle" />
-    <span class="hidden"></span>
-    <span class="hidden"></span>
-    <span class="hidden"></span>
+    <span></span>
+    <span></span>
+    <span></span>
     <!-- Dropdown dinámico -->
     <ul class="dropdown-menu">
       {#each routes as route}
       <li>
-        <a href={route.path} onclick={() => setActive(route.key)}>
+        <!-- <a href={route.path} onclick={() => setActive(route.key)}>
           {route.label}
+        </a> -->
+        <a 
+          class:active={isActive(route.path)}
+          aria-current={page.url.pathname === route.path ? 'page' : undefined}
+          href={route.path}>{route.label}
         </a>
-      </li>
+        </li>
       {/each}
     </ul>
   </label>
 </div>
+<a class="icono-perfil" href="/store-profile" aria-label="ir a perfil"
+  onclick={() => setActive("store-profile")}>
+  <i class="ph ph-user-circle user-img" aria-label="icono perfil"></i>
+</a>
 
-  <a class="icono-perfil" href="/store-profile" aria-label="ir a perfil"
-    onclick={() => setActive("store-profile")} class:active={page.url.pathname  === "/store-profile"}>
-    <i class="ph ph-user-circle user-img" aria-label="icono perfil"></i>
-  </a>
 
   <div class="mobile-icons">
-    <a href="/orders" aria-label="ir a orders"
-      onclick={() => setActive("orders")} class:active={page.url.pathname  === "/orders"}>
+    {#each routes as route}
+      <a 
+        href={route.path} 
+        aria-label={`ir a ${route.key}`}
+        class:active={isActive(route.path)}>
+        <i class={`ph ${route.icon} hidden`}></i>
+      </a>
+    {/each}
+    <!-- <a href="/orders" aria-label="ir a orders"
+      onclick={() => setActive("orders")} class:active={isActive('/orders')}>
       <i class="ph ph-shopping-cart hidden"></i>
     </a>
     <a href="/menu" aria-label="ir a menu"
-      onclick={() => setActive("menu")} class:active={page.url.pathname  === "/menu"}>
+      onclick={() => setActive("menu")} class:active={isActive('/menu')}>
       <i class="ph ph-fork-knife hidden"></i>
     </a>
     <a href="/ingredients" aria-label="ir a ingredientes"
-      onclick={() => setActive("ingredients")} class:active={page.url.pathname  === "/ingredients"}>
+      onclick={() => setActive("ingredients")} class:active={isActive('/ingredients')}>
       <i class="ph ph-bowl-food hidden"></i>
     </a>
     <a href="/store-profile" aria-label="ir a perfil"
-      onclick={() => setActive("store-profile")} class:active={page.url.pathname  === "/store-profile"}>
+      onclick={() => setActive("store-profile")} class:active={isActive('/store-profile')}>
       <i class="ph ph-user hidden"></i>
-    </a>
+    </a> -->
   </div>
 </nav>
 
@@ -111,18 +139,35 @@
     display: block;
   }
 
+  .activeLink{
+    display: none;
+  }
+
   /* ================================= */
 /* Desktop (>=769px) */
 @media (min-width: 769px) {
+  .options-and-icon a {
+  display: inline-flex;
+  }
   .dropdown {
     display: none;
+  }
+  .icono-perfil {
+    display: inline-flex;
   }
 }
 
 /* Tablet (431px – 768px) */
 @media (min-width: 431px) and (max-width: 768px) {
+  .options-and-icon > a {
+    display: none; /* oculto links de texto */
+  }
   .dropdown {
     display: inline-block;
+  }
+  .options-and-icon .icono-perfil,
+  .options-and-icon .activeLink {
+      display: inline-block;
   }
 }
 
@@ -133,6 +178,15 @@
   }
   .icono-perfil {
     display: none;
+  }
+  .options-and-icon > a {
+    display: none;
+  }
+  .mobile-icons{
+    display: inline-flex; 
+  }
+  .hidden{
+    display: inline-flex;
   }
 }
 </style>
