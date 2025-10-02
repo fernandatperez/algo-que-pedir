5d8f2c9a67d52aafd97c5ae8a2841b52c011bc96
Fernanda Perez <fernanda.tperez@outlook.com>
2025-10-02 12:56:08 -0300

    saco los componentes fieldset, vuelvo a los componentes input

--- src/routes/(authed)/store-profile/+page.svelte
+++ src/routes/(authed)/store-profile/+page.svelte
@@ -7,6 +7,8 @@
   import "$lib/css/components-css/buttons.css";
   import "$lib/css/components-css/input.css";
   import "$lib/css/pages-css/9-store-profile.css";
+  import Input from "$lib/components/Input.svelte";
+  import { InputTypes } from "$lib/components/InputTypes"
   
   // Components
   import FormFieldset from '$lib/FormFieldset.svelte';
@@ -73,68 +75,147 @@
 <main class="container-column">
   <article class="container-column main-content">
     <h1 class="header-title">Información del local</h1>
-
-    <form id="form-store-profile" class="container-column form-store-profile" onsubmit={handleSubmit} novalidate>
+     <form action="" id="form-store-profile" class="container-column form-store-profile">
+      <!-- Datos del Local  -->
+        <fieldset form="form-store-profile" name="store-info" class="content-section form-section-store-info">
+          <div class="grid-cols-2 input-group-dir " >
+            <div>
+                <div>
+                <Input
+                    description="Nombre del local"
+                    input_type={InputTypes.Normal}
+                    inputProps={{
+                    class: "input-primary",
+                    label: "Nombre del local*",
+                    name: "name",
+                    }}
+                />
+               </div>   
+                <div>
+                  <Input
+                    description="URL de la imagen*"
+                    input_type={InputTypes.Normal}
+                    inputProps={{
+                    class: "input-primary",
+                    label: "Imagen*",
+                    name: "name",
+                    }}/>
+            </div>
+            
+            </div>  
+            <div class="img-store-container"><img src="src\lib\assets\img\CarlosBakeShop.jpg" alt="local" class="img-store-profile"></div>  
+            
+          </div> 
+          
+          </fieldset>          
+          <!-- Direccion  -->
+          <fieldset form="form-store-profile" name="store-dir" class="container-column content-section">
+            <h2 class="subtitle">Dirección</h2>
+            <div class="grid-cols-2 input-group-dir ">
+            <div>
+              <Input
+                description="Direccion Local"
+                input_type={InputTypes.Normal}
+                inputProps={{
+                class: "input-primary",
+                label: "Dirección*",
+                name: "name",
+              }}/>
+            </div>
+            <div>
+              <Input
+                description="Altura"
+                input_type={InputTypes.Normal}
+                inputProps={{
+                class: "input-primary",
+                label: "Altura*",
+                name: "name",
+              }}/>
+           </div>  
+           <div>
+                            <Input
+                description="Latitud"
+                input_type={InputTypes.Normal}
+                inputProps={{
+                class: "input-primary",
+                label: "Latitud*",
+                name: "name",
+            }}/>
+          </div> 
+          <div>
+              <Input
+                description="Longitud"
+                input_type={InputTypes.Normal}
+                inputProps={{
+                class: "input-primary",
+                label: "Longitud*",
+                name: "name",
+            }}/>
+          </div> 
+          </div>
+        </fieldset>
+        <fieldset form="form-store-profile" name="store-comission" class="container-column content-section form-section-store-commission">
+        <h2 class="subtitle">Porcentajes</h2>
+        <div class="grid-cols-2 input-group-dir ">
+        <div class="container-column input-group">
+          <div class="grid-cols-2 input-group-dir ">
+              <Input
+                description="Porcentaje de comision con la app*"
+                input_type={InputTypes.Normal}
+                inputProps={{
+                class: "input-primary",
+                label: "Porcentaje de comision con la app*",
+                name: "name",
+            }}/>
+            </div>
       
-      <!-- Store Info -->
-      <FormFieldset
-         title={storeInfo.title}
-         name={storeInfo.name}
-         fields={storeInfo.fields}
-         bind:formData
-         section="storeInfo"
-         {errors}
-       />
-      
-      
-      <!-- Store Direction -->
-      <FormFieldset
-        title={storeDir.title}
-        name={storeDir.name}
-        fields={storeDir.fields}
-        bind:formData
-        section="storeDir"
-        {errors}
-      />
-
-      <!-- Store Commission -->
-      <FormFieldset
-        title={storeCommission.title}
-        name={storeCommission.name}
-        fields={storeCommission.fields}
-        bind:formData
-        section="storeCommission"
-        {errors}
-      />
-
-      <!-- Payment Methods -->
-      
-      <FormFieldset
-        title="Métodos de Pago"
-        name="payment-methods"
-        fields={paymentMethods}
-        bind:formData
-        section="paymentMethods"
-        {errors}
-      />
-      
-      
-      <!-- Buttons -->
-      <section class="btn-group-actions">
-        <button 
-          type="button" 
-          class="btn btn-secondary btn-store" 
-          onclick={handleDiscard}
-        >
-          Descartar <span class="p-cambios display-none-mobile">Cambios</span>
-        </button>
-        <button 
-          type="submit" 
-          class="btn btn-primary btn-store"
-        >
-          Guardar <span class="p-cambios display-none-mobile">Cambios</span>
-        </button>
-      </section>
-    </form>
+            <div class="container-column input-group">
+              <Input
+                description="Porcentaje de comision con autores de platos*"
+                input_type={InputTypes.Normal}
+                inputProps={{
+                class: "input-primary",
+                label: "Porcentaje de comision con autores de platos*",
+                name: "name",
+            }}/>
+            </div>                    
+            </div>
+            </div>
+          </fieldset>
+    
+         <fieldset form="form-store-profile" name="store-payment-methods" class="container-column content-section">
+          <h2 class="subtitle">Metodos de Pago</h2>
+          <div class="payments-checkbox-group">
+            <label for="efectivo" class="label-color">
+            <input type="checkbox" id="efectivo" 
+                   name="efectivo" 
+                   value="EFECTIVO"
+                   class="payment-checkbox">
+                   <span>Efectivo</span>
+            </label>
+        
+            <label for="qr" class="label-color">
+            <input type="checkbox" id="qr" 
+                   name="qr" 
+                   value="QR"
+                   class="payment-checkbox">
+                   <span>QR</span>
+            </label>
+    
+            <label for="transferencia" class="label-color">
+            <input type="checkbox" id="transferencia" 
+                   name="transferencia" 
+                   value="TRANSFERENCIA"
+                   class="payment-checkbox">
+                   <span>Transferencia</span>
+            </label>
+            </div>
+          </fieldset>
+                
+                <section class="btn-group-actions">
+                    <button disabled class="btn btn-secondary btn-store">Descartar <span class="p-cambios display-none-mobile">Cambios</span></button>
+                    <button disabled class="btn btn-primary btn-store">Guardar <span class="p-cambios display-none-mobile">Cambios</span></button>
+                </section>
+            </form>
   </article>
 </main>
