e6298eb2aef4fe71d377055eee3b3a18cb8cc732
nicky <nicolas_cernadas@hotmail.com>
2025-10-08 21:30:32 -0300

    Primer acercamiento APIs de /login

--- src/lib/services/UserService.ts
+++ src/lib/services/UserService.ts
@@ -1,5 +1,8 @@
-import { UserType } from '$lib/domain/user'
+import { UserType, type UserJSON } from '$lib/domain/user'
 import { USERS_LIST_MOCK } from '$lib/data/mock/users'
+import axios from 'axios'
+import { REST_SERVER_URL } from './configuration'
+import { getAxiosData } from './common'
 
 class UserService {
   async getAllUsers(){
@@ -7,10 +10,15 @@ class UserService {
   }
 
   async getUser(username: string, password: string) {
-    return USERS_LIST_MOCK.find(user => 
-      user.username !== undefined && 
-      user.username === username && 
-      user.password === password)
+    const queryUsuario = () => axios.post<UserJSON>(REST_SERVER_URL + '/login', {
+      'correo': username,
+      'contraseña': password
+    })
+    return UserType.fromJson(await getAxiosData(queryUsuario))
+    // return USERS_LIST_MOCK.find(user => 
+    //   user.username !== undefined && 
+    //   user.username === username && 
+    //   user.password === password)
   }
 
   async alreadyRegisteredUsername(username: string) {
