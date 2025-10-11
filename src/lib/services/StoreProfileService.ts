ec1ac6b7bf6b5db846ba02d0169edcb9220f3317
Fernanda Perez <fernanda.tperez@outlook.com>
2025-10-10 23:16:52 -0300

    cambio mock por axios

--- src/lib/services/StoreProfileService.ts
+++ src/lib/services/StoreProfileService.ts
@@ -1,14 +1,22 @@
-import { StoreType } from '$lib/domain/store'
+import { StoreType, type StoreJSON } from '$lib/domain/store'
 import { STORE_MOCK } from '$lib/data/mock/store'
+import { getAxiosData } from './common'
+import { REST_SERVER_URL } from './configuration'
+import axios from 'axios'
 
 class StoreService {
   async getStore(){
-    return STORE_MOCK.map(StoreType.fromJson)
+    const queryStore = () => axios.get<StoreJSON[]>(REST_SERVER_URL + '/store-profile')
+    return (await getAxiosData(queryStore)).map(StoreType.fromJson)
+    //return STORE_MOCK.map(StoreType.fromJson)
   }
+  
 
+  
   async updateStore(store: StoreType){
-    STORE_MOCK[0] = { ...store }
-    return STORE_MOCK[0]
+    return axios.put<StoreJSON>(REST_SERVER_URL + '/store-profile', store.toJSON())
+    //STORE_MOCK[0] = { ...store }
+    //return STORE_MOCK[0]
   }
 }
 
