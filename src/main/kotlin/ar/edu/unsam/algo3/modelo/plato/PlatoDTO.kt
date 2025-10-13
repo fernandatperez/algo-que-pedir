986640b79e69d06bc2c638a3bff49d2d822113cc
nicky <nicolas_cernadas@hotmail.com>
2025-10-13 20:52:33 -0300

    toDTO & fromDTO EM en PlatoDTO

--- src/main/kotlin/ar/edu/unsam/algo3/modelo/plato/PlatoDTO.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/modelo/plato/PlatoDTO.kt
@@ -1,6 +1,8 @@
 package ar.edu.unsam.algo3.modelo.plato
 
 import ar.edu.unsam.algo3.DTO.IngredienteDTO
+import ar.edu.unsam.algo3.DTO.toDOM
+import ar.edu.unsam.algo3.DTO.toDTO
 import ar.edu.unsam.algo3.mock.LocalPollos
 import ar.edu.unsam.algo3.modelo.local.Local
 
@@ -29,3 +31,30 @@ data class PlatoDTO(
     val ingredientes: MutableList<IngredienteDTO>,
     val store: Local = LocalPollos, // Esto deberia ser DTO tambien
 ) {}
+
+fun Plato.toDTO(): PlatoDTO {
+    return PlatoDTO(
+        id = this.id,
+        nombre = this.nombre,
+        descripcion = this.descripcion,
+        precio = this.valorVenta(),
+        imagen = this.urldeImagen,
+        costoProduccion = this.costoProduccion(),
+        esDeAutor = this.esDeAutor,
+        enPromocion = this.esNuevo(),
+        ingredientes = this.ingredientes.map{ it -> it.toDTO()}.toMutableList(),
+//        store = this.local.toResponse(), // Cuando tengamos lo mismo
+        store = this.local
+    )
+}
+
+fun Plato.fromDTO(platoDTO: PlatoDTO): Plato {
+    val ingredientesDom = platoDTO.ingredientes.map { it -> it.toDOM() }.toMutableList()
+    return Plato(
+        nombre = platoDTO.nombre,
+        descripcion = platoDTO.descripcion,
+        urldeImagen = platoDTO.imagen,
+        esDeAutor = platoDTO.esDeAutor,
+        ingredientes = ingredientesDom
+    )
+}
