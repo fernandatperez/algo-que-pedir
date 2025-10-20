49f5f476d7e04e8a69118208ccbfb8567fe6c623
dcreyes <dcossettinireyes@estudiantes.unsam.edu.ar>
2025-10-20 19:38:57 -0300

    ErrorHandler modificado y funcionando

--- src/lib/domain/errorHandler.ts
+++ src/lib/domain/errorHandler.ts
@@ -3,14 +3,32 @@ import { toasts } from '../components/toast/toastStore'
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const getErrorMessage = (error: any): string => {
   if (error.response && error.response.data) {
-    return error.response.data.status < 500 ? error.response.data.message : 'Ocurrió un error, consulte al administrador del sistema.'
+    const responseData = error.response.data
+    const status = error.response.status
+    if (typeof responseData === 'object' && responseData !== null){
+      if (responseData.detalle) {
+        return responseData.detalle
+      }
+
+      if(responseData.message && status < 500) {
+        return responseData.message
+      }
+      
+      if(status >= 500) {
+        return 'Ocurrió un error, consulte al administrador del sistema.'
+      }
+      
+    }
+    if (typeof responseData === 'string' && status < 500){
+      return responseData
+    }
+    
   } else if (error.code === 'ERR_NETWORK') {
     return 'Ocurrió un problema de conexión con el servidor. Intente nuevamente más tarde'
   } else if (error.message) {
     return error.message
-  } else {
-    return 'Error desconocido'
-  }
+  } return 'Error desconocido'
+ 
 }
 
 export const showError = (operation: string, error: unknown) => {
