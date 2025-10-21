d0ed9cc794029c118280a00cb45f3004a112f59e
CatalinaCorrea <catalinayazmincorrea@gmail.com>
2025-10-21 13:30:02 -0300

    arreglo antes del tag

--- src/main/kotlin/ar/edu/unsam/algo3/dto/pedidoDTO.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/dto/pedidoDTO.kt
@@ -42,29 +42,3 @@ fun Pedido.toDTO(): PedidoDTO {
     }
     return pedidoDTO
 }
-
-//fun PedidoDTO.fromDTO() : Pedido {
-//    val direccionUsuario = Direccion(
-//        calle = this.direccion,
-//        altura = this.altura,
-//        ubicacion = Point(lat.toDouble(), long.toDouble()),
-//    )
-//    val usuario = Usuario(
-//        nombre = this.nombre,
-//        username = this.username,
-//        direccion = direccionUsuario,
-//    )
-//
-//  val pedido = Pedido(
-//      usuario = usuario,
-//      local = Local(),
-////      platos = this.platos.map { it.fromDTO() }.toMutableList(),
-//      platos = this.platos.map { Plato().fromDTO(it) }.toMutableList(),
-//      medioDePagoElegido = this.metodoDePago,
-//      estado = this.estado,
-//    ).apply {
-//      this.id = this@fromDTO.id
-//      // this.horarioEntrega = this@fromDTO.horarioEntrega
-//  }
-//    return pedido
-//}
