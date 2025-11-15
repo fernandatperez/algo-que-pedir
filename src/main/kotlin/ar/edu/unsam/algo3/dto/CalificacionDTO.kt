ab6adaa84d25a22a29f4aac19a0b2d1608a84504
nicky <nicolas_cernadas@hotmail.com>
2025-11-15 19:23:32 -0300

    Cambio chico en Calificacion

--- src/main/kotlin/ar/edu/unsam/algo3/dto/CalificacionDTO.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/dto/CalificacionDTO.kt
@@ -3,14 +3,12 @@ package ar.edu.unsam.algo3.dto
 import ar.edu.unsam.algo3.modelo.usuario.Calificacion
 
 data class CalificacionDTO(
-    val id: Int,
     val rate: Int,
     val experienceDesc: String
 )
 
 fun Calificacion.toDTO(): CalificacionDTO {
     return CalificacionDTO(
-        id = 0,
         rate = this.puntaje,
         experienceDesc = this.comentario
     )
